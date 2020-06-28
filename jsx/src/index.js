082a17c286f4cacb499d4b31e29a6fae778b257b
atsushi.koga <stbe51@gmail.com>
2020-06-29 08:41:51 +0900

    JSXでJS変数を参照

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    const buttonText = 'Click Me!';
+    const style = {backgroundColor: 'grey', color: 'white'};
     return (
         <div>
             <div>
                 <label className="label" htmlFor="name">Enter name:</label>
                 <input id="name" type="text"/>
-                <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+                <button style={style}>{buttonText}</button>
             </div>
         </div>
     );
