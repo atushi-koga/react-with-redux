06f47c7fbd7d5339ae1b960f69e279df2a4e9beb
atsushi.koga <stbe51@gmail.com>
2020-06-28 16:46:57 +0900

    JSXでクラス属性やインラインCSSを記述

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hello</div>;
+    return (
+        <div>
+            <div>
+                <label className="label" htmlFor="name">Enter name:</label>
+                <input id="name" type="text"/>
+                <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+            </div>
+        </div>
+    );
 }
 
 ReactDOM.render(
