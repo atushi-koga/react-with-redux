cb43d40c69e67ad52babe553832797967a5969e5
atsushi.koga <stbe51@gmail.com>
2020-07-06 08:48:27 +0900

    feat: Commentアイコン画像をfakerで生成するようにした

--- components/src/components/Comment.js
+++ components/src/components/Comment.js
@@ -1,9 +1,10 @@
 import React from "react";
+import faker from "faker";
 
 export default (prop) => {
     return (
         <div className="comment">
-            <a href="/" className="avatar"><img alt="avatar"/></a>
+            <a href="/" className="avatar"><img alt="avatar" src={faker.image.avatar()}/></a>
             <div className="content">
                 <a href="/" className="author">{prop.author}</a>
                 <div className="metadata"><span className="date">{prop.post_at}</span></div>
