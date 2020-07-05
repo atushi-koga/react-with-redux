709b1f9de25e1d04733f1109f87039a7a59a18eb
atsushi.koga <stbe51@gmail.com>
2020-07-05 11:19:45 +0900

    fix: CommentコンポーネントのHTMLを修正

--- components/src/components/Comment.js
+++ components/src/components/Comment.js
@@ -3,9 +3,9 @@ import React from "react";
 export default (prop) => {
     return (
         <div className="comment">
-            <a className="avatar"><img src=""/></a>
+            <a href="/" className="avatar"><img alt="avatar"/></a>
             <div className="content">
-                <a className="author">{prop.author}</a>
+                <a href="/" className="author">{prop.author}</a>
                 <div className="metadata"><span className="date">{prop.post_at}</span></div>
                 <div className="text">{prop.comment}</div>
             </div>
