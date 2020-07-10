a6a0a3515be2193bd40df365cdc2bb1e5192d827
atsushi.koga <stbe51@gmail.com>
2020-07-10 09:10:03 +0900

    refactor: Componentに渡すプロパティをマルチラインで揃えた

--- components/src/components/Comments.js
+++ components/src/components/Comments.js
@@ -5,9 +5,21 @@ export default () => {
     return (
         <div className="ui comments">
             <h3 className="ui dividing header">Comments</h3>
-            <Comment author="Mike" post_at="Today at 08:00" comment="Hi I'm Mike"/>
-            <Comment author="Cathy" post_at="Today at 09:00" comment="Hi I'm Cathy"/>
-            <Comment author="Bob" post_at="Today at 10:00" comment="Hi I'm Bob"/>
+            <Comment
+                author="Mike"
+                post_at="Today at 08:00"
+                comment="Hi I'm Mike"
+            />
+            <Comment
+                author="Cathy"
+                post_at="Today at 09:00"
+                comment="Hi I'm Cathy"
+            />
+            <Comment
+                author="Bob"
+                post_at="Today at 10:00"
+                comment="Hi I'm Bob"
+            />
         </div>
     );
 }
