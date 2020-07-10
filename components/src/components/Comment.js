0de6c4dced5da4df051f60e37ac862c28bc93c3a
atsushi.koga <stbe51@gmail.com>
2020-07-10 09:14:09 +0900

    refactor: Component内で画像URLを生成していたのを外から渡すようにした

--- components/src/components/Comment.js
+++ components/src/components/Comment.js
@@ -1,10 +1,9 @@
 import React from "react";
-import faker from "faker";
 
 export default (prop) => {
     return (
         <div className="comment">
-            <a href="/" className="avatar"><img alt="avatar" src={faker.image.avatar()}/></a>
+            <a href="/" className="avatar"><img alt="avatar" src={prop.avatar}/></a>
             <div className="content">
                 <a href="/" className="author">{prop.author}</a>
                 <div className="metadata"><span className="date">{prop.post_at}</span></div>
--- components/src/components/Comments.js
+++ components/src/components/Comments.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Comment from "./Comment";
+import faker from "faker";
 
 export default () => {
     return (
@@ -9,16 +10,19 @@ export default () => {
                 author="Mike"
                 post_at="Today at 08:00"
                 comment="Hi I'm Mike"
+                avatar={faker.image.avatar()}
             />
             <Comment
                 author="Cathy"
                 post_at="Today at 09:00"
                 comment="Hi I'm Cathy"
+                avatar={faker.image.avatar()}
             />
             <Comment
                 author="Bob"
                 post_at="Today at 10:00"
                 comment="Hi I'm Bob"
+                avatar={faker.image.avatar()}
             />
         </div>
     );
