a8ca8113458fe51bd22580607747ffb06c75d134
atsushi.koga <stbe51@gmail.com>
2020-07-15 21:06:20 +0900

    refactor: Functional Component を Class Based Component に変更

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
-    )
-    return (
-        <div>
-            <div>Hello World</div>
-            <div><SeasonDisplay/></div>
-        </div>
-    );
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        )
+        return (
+            <div>
+                <div>Hello World</div>
+                <div><SeasonDisplay/></div>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'))
