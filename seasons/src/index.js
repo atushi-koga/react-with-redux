385e1deedb9a2c054fddd8ac792afcc565caec93
atsushi.koga <stbe51@gmail.com>
2020-07-15 09:33:17 +0900

    feature: Geolocation APIを使ってユーザの現在地を取得する

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (error) => console.log(error)
+    )
     return (
         <div>
             <div>Hello World</div>
