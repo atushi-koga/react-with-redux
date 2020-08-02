80c921a0778eb169ea79dd3b0b2ce748bc7b46f6
atsushi.koga <stbe51@gmail.com>
2020-08-02 11:35:38 +0900

    feat: stateの値をpropsとして子コンポーネントに渡す

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>
+const getSeason = (latitude, month) => {
+    if (month => 3 && month <= 9) {
+        return latitude > 0 ? 'summer' : 'winter';
+    } else {
+        return latitude > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    return <div>{season}</div>
 }
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
     state = {lat: null, errorMessage: ''};
@@ -9,15 +10,16 @@ class App extends React.Component {
             position => this.setState({lat: position.coords.latitude}),
             error => this.setState({errorMessage: error.message})
         );
+        console.log(this.state);
     }
 
     render() {
-        if (this.state.lat && !this.state.errorMessage) {
-            return <div>latitude: {this.state.lat}</div>
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div>Error: {this.state.errorMessage}</div>
         }
 
-        if (!this.state.lat && this.state.errorMessage) {
-            return <div>errorMessage: {this.state.errorMessage}</div>
+        if (this.state.lat && !this.state.errorMessage) {
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
         return <div>Loading...</div>
