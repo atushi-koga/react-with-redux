394df9360cbb2f95e0a9517772a8a6118b8921df
koga <koga@wh-plus.com>
2020-08-25 20:00:44 +0900

    feat: ダミーのTaskリストを画面に表示

--- todo/src/components/Tasks.js
+++ todo/src/components/Tasks.js
@@ -1,9 +1,31 @@
 import React from "react";
+import {connect} from "react-redux";
+import {fetchTasks} from "../actions";
 
 class Tasks extends React.Component {
+    componentDidMount() {
+        this.props.fetchTasks();
+    }
+
+    renderTasks() {
+        return this.props.tasks.map(task => {
+            return (
+                <div key={task.id}>
+                    <div>{task.id}</div>
+                    <div>{task.body}</div>
+                    <div>{task.title}</div>
+                </div>
+            );
+        })
+    }
+
     render() {
-        return <div>TodoList</div>;
+        return <div>{this.renderTasks()}</div>;
     }
 }
 
-export default Tasks;
+const mapStateToProps = state => {
+    return {tasks: state.tasks}
+}
+
+export default connect(mapStateToProps, {fetchTasks})(Tasks);
