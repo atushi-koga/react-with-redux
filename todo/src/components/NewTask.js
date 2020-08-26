75340a1312f088b70230a497ff9b70b078c1b8c9
koga <koga@wh-plus.com>
2020-08-26 17:03:26 +0900

    feat: 入力値をもとに新規Taskを登録

--- todo/src/components/NewTask.js
+++ todo/src/components/NewTask.js
@@ -3,18 +3,35 @@ import {connect} from "react-redux";
 import {createTask} from "../actions";
 
 class NewTask extends React.Component {
+    state = {
+        title: '',
+        body: ''
+    }
+
     render() {
         return (
             <div>
                 <div>New Task</div>
                 <div>
-                    <input placeholder="title"/>
+                    <input
+                        onChange={e => this.setState({title: e.target.value})}
+                        placeholder="title"
+                    />
                 </div>
                 <br/>
                 <div>
-                    <textarea placeholder="body"/>
+                    <textarea
+                        onChange={e => this.setState({body: e.target.value})}
+                        placeholder="body"
+                    />
                 </div>
-                <div><button onClick={() => this.props.createTask('new title', 'new body')}>登録</button></div>
+                <div>
+                    <button
+                        onClick={() => this.props.createTask(this.state.title, this.state.body)}
+                    >
+                        登録
+                    </button>
+                </div>
             </div>
         );
     }
