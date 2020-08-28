fae40faf6a3e28053ae7fc5e28553199196e90ff
koga <koga@wh-plus.com>
2020-08-28 12:29:08 +0900

    feat: Accordion item を表示

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,26 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end Javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineer'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    },
+];
 
 export default () => {
-    return <div>App</div>
+    return (
+        <div>
+            <br/>
+            <Accordion items={items}/>
+        </div>
+    )
 }
