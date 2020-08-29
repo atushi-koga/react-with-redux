68f289b9ef7611a5a45349d6591548e9a512de64
koga <koga@wh-plus.com>
2020-08-29 11:04:30 +0900

    feat: wikipeidaのページに遷移するボタンを作成

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -30,9 +30,17 @@ const Search = () => {
         return results.map(result => {
             return (
                 <div className="item" key={result.pageid}>
+                    <div className="right floated content">
+                        <a
+                            href={`https://en.wikipedia.org/?curid=${result.pageid}`}
+                            className="ui button"
+                        >
+                            Go
+                        </a>
+                    </div>
                     <div className="content">
                         <div className="header">title: {result.title}</div>
-                        {result.snippet}
+                        <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
                     </div>
                 </div>
             );
