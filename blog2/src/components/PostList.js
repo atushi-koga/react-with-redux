5dc0b235649f471da6dae7d151c17e2687168075
koga <koga@wh-plus.com>
2022-01-10 11:35:21 +0900

    doc: コメント追加

--- blog2/src/components/PostList.js
+++ blog2/src/components/PostList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 
+// 関数コンポーネントに書き換える
 class PostList extends React.Component {
     componentDidMount() {
         this.props.fetchPosts();
