51245e91c0994ed0dc514bbca5f44c12ae76620f
koga <koga@wh-plus.com>
2022-01-13 20:06:30 +0900

    refactor: コンポーネントのpropsがデータを持ちすぎているので、必要な分だけ持つようにロジックをmapStateToPropsに抽出

--- blog2/src/components/UserHeader.js
+++ blog2/src/components/UserHeader.js
@@ -8,16 +8,15 @@ class UserHeader extends React.Component {
     }
 
     render() {
-        const user = this.props.users.find(user => user.id === this.props.userId);
-        if (!user) {
+        if (!this.props.user) {
             return null;
         }
-        return <div>{user.name}</div>
+        return <div>{this.props.user.name}</div>
     }
 }
 
-const mapStateToProps = state => {
-    return {users: state.users}
+const mapStateToProps = (state, ownProps) => {
+    return {user: state.users.find(user => user.id === ownProps.userId)}
 }
 
 export default connect(mapStateToProps, {fetchUser})(UserHeader);
